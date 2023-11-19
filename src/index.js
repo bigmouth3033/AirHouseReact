634a9ea19a9d16ffa13dad79a9b155d952a3fd80
tan <bigmouth3033@gmail.com>
2023-11-19 16:43:50 +0700

    first test sucess

--- src/index.js
+++ src/index.js
@@ -8,14 +8,21 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { Suspense } from "react";
 import Loading from "./components/Loading";
+import Admin from "./pages/Admin";
+import { Provider } from "react-redux";
+import store from "store";
+import AdminLayout from "layouts/AdminLayout";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ContextProvider>
-    <Suspense fallback={<Loading />}>
-      <RouterProvider router={router} />
-    </Suspense>
+      <Suspense fallback={<Loading />}>
+        <RouterProvider router={router} />
+      </Suspense>
   </ContextProvider>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/layouts/AdminLayout.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { AppContent, AppSidebar, AppFooter, AppHeader } from "components/admin/index";
+import { CContainer, CSpinner } from "@coreui/react";
+import { Outlet } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../store";
+import { Suspense } from "react";
+
+export default function AdminLayout() {
+  return (
+    <Provider store={store}>
+      <Suspense>
+        <AppSidebar />
+        <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+          <AppHeader />
+          <div className="body flex-grow-1 px-3">
+            <CContainer lg>
+              <Suspense fallback={<CSpinner color="primary" />}>
+                <Outlet />
+              </Suspense>
+            </CContainer>
+          </div>
+          <AppFooter />
+        </div>
+      </Suspense>
+    </Provider>
+  );
+}
