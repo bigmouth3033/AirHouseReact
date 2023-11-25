31078d487a456f69ad73063c7b6787308befe8c8
mainguyen <nguyenthingocmai193.com>
2023-11-25 20:47:57 +0700

    text editor

--- src/views/blog/BlogList.js
+++ src/views/blog/BlogList.js
@@ -1,49 +1,33 @@
-import React from "react";
-import styled from "styled-components";
-import "bootstrap/dist/css/bootstrap.css";
+import React, { useState } from "react";
+import "react-quill/dist/quill.snow.css";
+import ReactQuill, { Quill } from "react-quill";
+import "quill-mention";
 
-const StyledTable = styled.div`
-  color: red;
-
-  &.colspan-2 {
-    grid-column: span 2;
-  }
-`;
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ size: ["12", "18"] }],
+    [{ list: "order" }, { list: "bullet" }, { indent: "-1" }, { indent: "1" }],
+    ["link", "image", "video"],
+  ],
+};
 
 export default function BlogList() {
+  const [value, setValue] = useState("");
   return (
-    <StyledTable>
-      <table className="table table-hover table-striped text-center">
-        <thead>
-          <tr>
-            <td>
-              <b>ID</b>
-            </td>
-            <td>
-              <b>Title</b>
-            </td>
-            <td>
-              <b>Created Date</b>
-            </td>
-            <td colSpan={2}>
-              <b>Action</b>
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>hello</td>
-            <td>hey</td>
-            <td>heyheyhey</td>
-            <td>
-              <button className="btn btn-secondary ">Update</button>
-            </td>
-            <td >
-              <button className="btn btn-danger">Delete</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </StyledTable>
+    <>
+      <ReactQuill
+        theme="snow"
+        placeholder="Write today blog..."
+        value={value}
+        onChange={setValue}
+        modules={modules}
+        style={{ height: "40vh" }}
+      />
+      <div>{value}</div>
+      <div dangerouslySetInnerHTML={{ __html: value }} />
+    </>
   );
 }
